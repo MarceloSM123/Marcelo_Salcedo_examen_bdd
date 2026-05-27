8932a03316cd0d96e754b036e59ed3095bfb9fe0
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-26 20:21:08 -0500

    LMSM: Parte 1: PostgreSQL Básico y Manipulación de Datos implementacion de la tabla vuelos

---
+++ parte1_vuelos.sql
@@ -0,0 +1,20 @@
+
+-- PARTE 1: PostgreSQL Básico y Manipulación de Datos
+
+
+-- CREAR BASE DE DATOS
+CREATE DATABASE examen_vuelos;
+
+-- CREAR TABLA vuelos
+
+CREATE TABLE vuelos (
+    id SERIAL PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    origen VARCHAR(50) NOT NULL,
+    destino VARCHAR(50) NOT NULL,
+    fecha_salida TIMESTAMP NOT NULL,
+    asientos_totales INTEGER NOT NULL CHECK (asientos_totales > 0),
+    asientos_disponibles INTEGER NOT NULL CHECK (asientos_disponibles >= 0),
+    precio_boleto DECIMAL(10,2) NOT NULL CHECK (precio_boleto > 0)
+);
+
